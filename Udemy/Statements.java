ba22b3519a58b3f3b8046daab81b5e65c0417756
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-02-16 23:41:55 +0600

    Add files via upload

---
+++ Udemy/Statements.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Udemy;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class Statements {
+        public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter a Number:");
+        int n = sc.nextInt();
+        
+        if(n%2==0){
+            System.out.println("EVEN number..");
+        }else{
+            System.out.println("ODD number..");
+        }
+        
+        System.out.println("Enter a Value:");
+        int num = sc.nextInt();
+        
+        if(num == 5){
+            System.out.println("The numbre is 5");
+        }else{
+            System.out.println("The number is "+num);
+        }
+        
+        System.out.println("Enter your age ");
+        int age = sc.nextInt();
+        
+        if(age >= 18 && age <= 40 ){
+            System.out.println("You will get the license and job as well..");
+        }
+        else if(age > 40 || age <  18 && age >0){
+            System.out.println("You are not eligible..");
+        }else{
+            System.out.println("Wrong input..");
+        }
+    }
+}
